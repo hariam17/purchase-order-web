e9923adcbed7f467e3fb1c8c57ed988384025470
hariam <hariagung1703@gmail.com>
2023-09-21 13:36:50 +0700

    Assignment Day 4 Maxy Academy

--- app/Http/Controllers/Admin/PurchaseOrderController.php
+++ app/Http/Controllers/Admin/PurchaseOrderController.php
@@ -43,10 +43,12 @@ class PurchaseOrderController extends Controller
         
     }
     public function getPurchaseOrderLineCreate(){
-        return view('admin.purchaseOrderLines.create');
+        $products = Product::all();
+        return view('admin.purchaseOrderLines.create', ["products" => $products]);
     }
     public function postPurchaseOrderLineInsert(Request $request, PurchaseOrderLine $purchaseOrderLine){
        $validator = Validator::make($request->all(), [
+        'product' => 'required',
         'qty' => 'required',
         'price' => 'required',
         'discount' => 'required',
@@ -54,6 +56,7 @@ class PurchaseOrderController extends Controller
 
        if ($validator->fails()) return redirect()-> back()->withErrors($validator->errors());
        
+       $purchaseOrderLine->product_id = $request->post('product');
        $purchaseOrderLine->qty = $request->post('qty');
        $purchaseOrderLine->price = $request->post('price');
        $purchaseOrderLine->discount = $request->post('discount');
